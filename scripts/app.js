e807872203ca72d85e601efef6a3f1012e833eef
Ethan Gould <ethan.g.907@gmail.com>
2014-10-15 21:37:35 -0400

    added songs to respective playlists

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,21 @@ var app = angular.module('Playlistr', []);
 
 app.controller('PlaylistsController', function(){
 
-  this.songs = [
-    {artist: 'Wiz Khalifa', title: 'Black and Yellow'},
-    {artist: 'Mac Miller', title: 'Frozen Pizza and Koolaid'},
-    {artist: 'Michael Jackson', title: 'Thriller'},
-    {artist: 'Outkast', title: 'Mrs. Jackson'}
+  this.playlists = [
+    {name: 'Pump Up Tunz', songs: [
+        {artist: 'Wiz Khalifa', title: 'Black and Yellow'},
+        {artist: 'Mac Miller', title: 'Frozen Pizza and Koolaid'}
+      ]
+    },
+    {name: 'Sunday Night Slow Jamz', songs: [
+        {artist: 'Michael Jackson', title: 'Thriller'},
+        {artist: 'Outkast', title: 'Mrs. Jackson'}
+      ]
+    },
+    {name: 'Workout Music', songs: [
+        {artist: 'Emeniem', title: 'Collapse'},
+        {artist: '50 Cent', title: 'Heat'}
+      ]
+    }
   ];
-
 });
