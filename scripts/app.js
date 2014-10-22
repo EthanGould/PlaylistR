52286173eb5808607d970a7bace5c8c17e06c923
EthanGould <ethan.g.907@gmail.com>
2014-10-22 12:27:48 -0400

    removed id attr from playlist objects, updates to setPlaylist function

--- scripts/app.js
+++ scripts/app.js
@@ -25,25 +25,27 @@ app.config(['$routeProvider', function($routeProvider) {
 app.controller('PlaylistsController', ['$scope', function($scope) {
 
   $scope.playlists = [
-    {id: 0, name: 'Pump Up Tunz', link: 'pump-up-tunz',songs: [
+    {name: 'Pump Up Tunz', link: 'pump-up-tunz', songs: [
         {artist: 'Wiz Khalifa', title: 'Black and Yellow'},
         {artist: 'Mac Miller', title: 'Frozen Pizza and Koolaid'}
       ]
     },
-    {id: 1, name: 'Sunday Night Slow Jamz', link: 'sunday-night-slow-jamz',songs: [
+    {name: 'Sunday Night Slow Jamz', link: 'sunday-night-slow-jamz', songs: [
         {artist: 'Michael Jackson', title: 'Thriller'},
         {artist: 'Outkast', title: 'Mrs. Jackson'}
       ]
     },
-    {id: 3, name: 'Workout Music', link: 'workout-music',songs: [
+    {name: 'Workout Music', link: 'workout-music', songs: [
         {artist: 'Emeniem', title: 'Collapse'},
         {artist: '50 Cent', title: 'Heat'}
       ]
     }
   ];
 
-  this.setPlaylist = function() {
-    alert("happy day");
+  this.setPlaylist = function(playlist) {
+    debugger;
+    this.currentPlaylist = playlist;
+    console.log(this.currentPlaylist);
   };
 
 }]);
