9cfd81513f9c1d1674fa8988b6064041015095f2
Ethan Gould <ethan.g.907@gmail.com>
2014-11-22 11:21:33 -0500

    setup Firebase linking new songs added to a playlist

--- scripts/controllers/playlistController.js
+++ scripts/controllers/playlistController.js
@@ -3,54 +3,48 @@ angular.module('Playlistr')
 .controller('PlaylistContentController', ['$firebase', '$scope', function($firebase, $scope) {
 
   var ref = new Firebase('https://playlistrapp.firebaseio.com/');
-  var sync = $firebase(ref);
-  $scope.fbPlaylistSongs = sync.$asArray();
 
   var clientId = '22aa56e479e5b0a4968c22120c32bde8';
   SC.initialize({
     client_id: clientId,
   });
 
-  $scope.addSong = function(track_url, playlist) {
+  $scope.getSong = function(track_url, playlist) {
     SC.get('/resolve', { url: track_url }, function(t) {
       var artwork = t.artwork_url;
       var defaultArtwork = 'http://findnewjams.com/frontend/img/default-album-art.png';
       if (t.artwork_url === null) {
         artwork = defaultArtwork;
       }
-      setTimeout( function(){
-        var songDuration = (t.duration/1000)/60;
-        var songDuration = parseFloat(parseFloat(songDuration).toFixed(1));
-        var newSong = {track_id: t.id,
-          artist: t.label_name,
-          title: t.title,
-          duration: songDuration,
-          artwork: artwork,
-          track_uri: t.uri,
-          download_count: t.download_count,
-          comment_count: t.comment_count,
-          favorite_count: t.favoritings_count
-        };
+      var songDuration = (t.duration/1000)/60;
+      var songDuration = parseFloat(parseFloat(songDuration).toFixed(1));
+      var newSong = {track_id: t.id,
+        artist: t.label_name,
+        title: t.title,
+        duration: songDuration,
+        artwork: artwork,
+        track_uri: t.uri,
+        download_count: t.download_count,
+        comment_count: t.comment_count,
+        favorite_count: t.favoritings_count
+      };
 
-        playlist.songs.push(newSong);
-        playlist.songCount += 1;
-        playlist.duration += songDuration;
-        $scope.$apply();
-        sendSongToPlaylist(newSong);
-      }, 10);
-      console.log(t);
+      // playlist.songs.push(newSong);
+      // playlist.songCount += 1;
+      // playlist.duration += songDuration;
+      // $scope.$apply();
+      // sendSongToFirebase(newSong);
+      sendSongToFirebase(newSong,playlist);
     });
     $('.song-input').val('');
   };
 
-  var sendSongToPlaylist = function(song) {
-    var ref = new Firebase('https://playlistrapp.firebaseio.com/playlists/-JaGQ1aUkVNPWz0b53jZ/');
-    var songsRef = ref.child('songs');
-    // remove hashkey for Firebase and send
-    delete song['$$hashKey']
-    // songsRef.push(song);
-    // var songID = songsRef.key();
-    // debugger;
+  var sendSongToFirebase = function(song, playlist) {
+    console.log(playlist.$id)
+    var singlePl = $firebase(ref.child('playlists/'+playlist.$id+'/songs')).$asArray();
+    singlePl.$add(song);
+    debugger;
+    console.log('New Song: ',song, 'Added to: ', playlist.name);
   }
 
   $scope.removeSong = function(song, playlist) {
